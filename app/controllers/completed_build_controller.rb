73227611eecc4e797f9a6dd1a21071832306abd9
kirangp <kirangp@gmail.com>
2016-07-11 20:08:37 +1000

    Added new methods in CompletedBuildController Added views for CompletedBuildController

--- app/controllers/completed_build_controller.rb
+++ app/controllers/completed_build_controller.rb
@@ -5,4 +5,39 @@ class CompletedBuildController < ApplicationController
     erb :'builds/index'
   end
 
+  get "/builds/new" do
+    redirect_if_not_logged_in
+
+    erb :'builds/new'
+  end
+
+  get "/builds/:id/edit" do
+    redirect_if_not_logged_in
+
+    @build = CompletedBuild.find(params[:id])
+    erb :'builds/edit'
+  end
+
+  post "/builds/:id" do
+    redirect_if_not_logged_in
+    @build = CompletedBuild.find(params[:id])
+
+
+    @build.update(params.select{|k| k=="name" || k=="price"})
+    redirect "/builds/#{@build.id}"
+  end
+
+  get "/builds/:id" do
+    redirect_if_not_logged_in
+    @build = CompletedBuild.find(params[:id])
+    erb :'builds/show'
+  end
+
+  post "/builds" do
+    redirect_if_not_logged_in
+    
+    CompletedBuild.create(params)
+    redirect "/builds"
+  end
+
 end
